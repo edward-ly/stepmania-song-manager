c8be1e30596f716dd0d002446b8cbc8ed26e1dda
Edward Ly <edward.ly@pm.me>
2021-03-06 13:28:05 +0900

    Added "Add Songs" button

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,31 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page class="q-pa-lg">
+    <div class="row">
+      <div class="col" />
+      <div class="col-auto">
+        <q-btn
+          to="/add"
+          no-wrap
+          no-caps
+          color="positive"
+          icon="add"
+          label="Add Songs"
+          class="btn-icon-left-padding-sm"
+          size="md"
+          padding="xs md xs sm"
+        />
+      </div>
+    </div>
+    <div class="row q-pt-lg">
+      <div class="col">
+        <div class="flex flex-center">
+          <img
+            alt="Quasar logo"
+            src="~assets/quasar-logo-full.svg"
+          >
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -12,3 +34,9 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({})
 </script>
+
+<style lang="scss">
+  .btn-icon-left-padding-sm .on-left {
+    margin-right: map-get($space-sm, "x");
+  }
+</style>
