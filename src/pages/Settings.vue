0b8e7accbbe4b349c3c7aba435287857e7f47c9d
Edward Ly <edward.ly@pm.me>
2021-03-07 15:11:19 +0900

    Added ability to select download path from dialog

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -9,9 +9,9 @@
       <q-separator inset />
 
       <q-card-section>
-        <q-input dense outlined v-model="downloadPath" @update:modelValue="updateDownloadPath">
+        <q-input dense outlined v-model="downloadPath" @update:modelValue="saveDownloadPath">
           <template v-slot:append>
-            <q-icon name="folder_open" @click.stop="openDir()" class="cursor-pointer" />
+            <q-icon name="folder_open" @click.stop="openDownloadPath()" class="cursor-pointer" />
           </template>
         </q-input>
       </q-card-section>
@@ -26,19 +26,25 @@ import { useQuasar } from 'quasar';
 export default defineComponent({
   setup () {
     const $q = useQuasar()
+    let downloadPath = ref($q.localStorage.getItem('DownloadPath'))
 
-    function updateDownloadPath (newPath) {
+    function saveDownloadPath (newPath) {
       $q.localStorage.set('DownloadPath', newPath)
     }
 
-    function openDir () {
-
+    function openDownloadPath () {
+      const result = window.electron.openFolderDialog(this.downloadPath)
+      if (result !== undefined) {
+        const newPath = result[0]
+        this.downloadPath = newPath
+        saveDownloadPath(newPath)
+      }
     }
 
     return {
-      downloadPath: ref($q.localStorage.getItem('DownloadPath')),
-      updateDownloadPath,
-      openDir
+      downloadPath,
+      saveDownloadPath,
+      openDownloadPath
     }
   }
 })
