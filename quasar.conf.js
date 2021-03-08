9035097376f0a86de449f1f61c7219087d2775c0
Edward Ly <edward.ly@pm.me>
2021-03-09 02:19:37 +0900

    Switched to electron-builder

--- quasar.conf.js
+++ quasar.conf.js
@@ -169,7 +169,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
     electron: {
-      bundler: 'packager', // 'packager' or 'builder'
+      bundler: 'builder', // 'packager' or 'builder'
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
