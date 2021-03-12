0ed1dd242a5f49cbe0f2c2d9bdbe065aff88d4d3
Edward Ly <edward.ly@pm.me>
2021-03-13 01:26:42 +0900

    Built add songs page

--- src/pages/AddSongs.vue
+++ src/pages/AddSongs.vue
@@ -1,22 +1,70 @@
 <template>
   <q-page class="q-pa-lg">
-    <!-- TODO: build page -->
-    <!-- TODO: build suggested repos list -->
-    <!-- TODO: add dialog for custom repo -->
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
+    <div class="row">
+      <div class="col-auto">
+        <div class="text-h5 q-my-none">Select a Repository</div>
+      </div>
+      <div class="col" />
+      <div class="col-auto q-my-auto">
+        <q-btn
+          no-wrap
+          no-caps
+          color="primary"
+          icon="add"
+          label="Add Custom Repo"
+          class="btn-icon-left-padding-sm"
+          size="md"
+          padding="xs md xs sm"
+          @click="addCustomRepo"
+        />
+      </div>
+    </div>
+    <div class="row q-pt-lg">
+      <div class="col">
+        <RepositoryCard
+          v-for="repo in defaultRepos"
+          :key="repo.url"
+          v-bind="repo"
+          :route="this.$route.path"
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import { useQuasar } from 'quasar'
+import RepositoryCard from 'components/RepositoryCard.vue'
+import defaultRepos from 'components/defaultRepos'
 
-export default defineComponent({})
+export default defineComponent({
+  components: {
+    RepositoryCard,
+  },
+
+  setup () {
+    const $q = useQuasar()
+
+    function addCustomRepo () {
+      // TODO: add dialog for new custom repo
+      $q.dialog({
+        title: 'New Repository',
+        prompt: {
+          model: '',
+          type: 'text' // optional
+        },
+        cancel: true,
+        persistent: true
+      }).onOk(data => {
+        console.log('>>>> OK, received', data)
+      })
+    }
+
+    return {
+      defaultRepos,
+      addCustomRepo,
+    }
+  },
+})
 </script>
