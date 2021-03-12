7dfc32f16261ec67f8abbdce2a0336f602cd524c
Edward Ly <edward.ly@pm.me>
2021-03-13 01:22:21 +0900

    Added default repos list

---
+++ src/components/defaultRepos.js
@@ -0,0 +1,14 @@
+const defaultRepos = [
+  {
+    name: 'DanceDanceRevolution (Arcade, Latest)',
+    url: 'https://gitlab.com/led_light/ddr-simfiles',
+    description: 'Simfiles for all songs in the latest official arcade release of DDR. Will include new songs, song fixes, and songs removals as the arcade game is updated.',
+  },
+  {
+    name: 'The Complete led_light Collection',
+    url: 'https://gitlab.com/led_light/simfiles',
+    description: 'DDR custom simfiles made by led_light (the creator of this app!). Includes a wide variety of songs such as BEMANI crossovers, K-Pop, video game OSTs, and everything in between!',
+  },
+]
+
+export default defaultRepos
