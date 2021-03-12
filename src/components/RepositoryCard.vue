161d3206614e16a64e788d1950094c82d138b941
Edward Ly <edward.ly@pm.me>
2021-03-13 01:25:00 +0900

    Added repository card component

---
+++ src/components/RepositoryCard.vue
@@ -0,0 +1,149 @@
+<template>
+  <q-card class="q-mb-lg">
+    <q-card-section>
+      <div class="text-h6">{{ name }}</div>
+      <div class="text-caption">{{ url }}</div>
+      <div v-if="description.length" class="text-body2 q-pt-sm">{{ description }}</div>
+      <div v-else class="text-body2 text-italic text-muted q-pt-sm">No description set.</div>
+    </q-card-section>
+
+    <q-separator inset />
+
+    <q-card-section v-if="route === '/'" class="row">
+      <q-space />
+      <q-btn
+        v-if="isDownloaded"
+        no-wrap
+        no-caps
+        color="primary"
+        icon="sync"
+        label="Update"
+        class="btn-icon-left-padding-sm q-ml-sm"
+        size="md"
+        padding="xs md xs sm"
+        @click="pullRepo"
+      />
+      <q-btn
+        v-else
+        no-wrap
+        no-caps
+        color="primary"
+        icon="download"
+        label="Download"
+        class="btn-icon-left-padding-sm q-ml-sm"
+        size="md"
+        padding="xs md xs sm"
+        @click="cloneRepo"
+      />
+      <q-btn
+        no-wrap
+        no-caps
+        color="accent"
+        icon="list"
+        label="View Song List"
+        class="btn-icon-left-padding-sm q-ml-sm"
+        size="md"
+        padding="xs md xs sm"
+        @click="getSongListLocal"
+      />
+      <q-btn
+        no-wrap
+        no-caps
+        color="negative"
+        icon="delete"
+        label="Delete"
+        class="btn-icon-left-padding-sm q-ml-sm"
+        size="md"
+        padding="xs md xs sm"
+        @click="deleteRepo"
+      />
+    </q-card-section>
+    <q-card-section v-else-if="route === '/add'" class="row">
+      <q-space />
+      <q-btn
+        no-wrap
+        no-caps
+        color="accent"
+        icon="list"
+        label="View Song List"
+        class="btn-icon-left-padding-sm q-ml-sm"
+        size="md"
+        padding="xs md xs sm"
+        @click="getSongListRemote"
+      />
+      <q-btn
+        v-if="!isDownloaded"
+        no-wrap
+        no-caps
+        color="positive"
+        icon="add"
+        label="Add"
+        class="btn-icon-left-padding-sm q-ml-sm"
+        size="md"
+        padding="xs md xs sm"
+        @click="addRepo"
+      />
+      <q-btn
+        v-else
+        no-wrap
+        no-caps
+        disable
+        color="positive"
+        icon="done"
+        label="Added"
+        class="btn-icon-left-padding-sm q-ml-sm"
+        size="md"
+        padding="xs md xs sm"
+      />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    route: {
+      type: String,
+      required: true,
+    },
+    isDownloaded: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    addRepo () {
+      // TODO: add repo to local list and redirect to home page
+    },
+    cloneRepo () {
+      // TODO: clone repo from remote
+    },
+    pullRepo () {
+      // TODO: pull latest commits from remote
+    },
+    deleteRepo () {
+      // TODO: delete repo from local list
+    },
+    getSongListLocal () {
+      // TODO: display song list from local song list file
+    },
+    getSongListRemote () {
+      // TODO: display song list from remote song list file
+    },
+  },
+})
+</script>
