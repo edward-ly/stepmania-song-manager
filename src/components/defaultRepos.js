8efafee99655fd86cb13aae38773d6776a659ec2
Edward Ly <edward.ly@pm.me>
2021-03-24 23:07:01 +0900

    Updated bucket descriptions

--- src/components/defaultRepos.js
+++ src/components/defaultRepos.js
@@ -3,19 +3,19 @@ const defaultRepos = [
     name: 'DanceDanceRevolution (Arcade, Latest)',
     bucketName: 'ddr-arcade-simfiles',
     description:
-      'A collection of simfiles containing every song in the latest official arcade release of DDR. Will include new songs, song fixes, and songs removals as the arcade game is updated.',
+      'A collection of simfiles containing every song in the latest official arcade release of DDR. Will include new songs, song fixes, songs removals, etc. as the arcade game is updated.',
   },
   {
     name: 'The Complete led_light Collection',
     bucketName: 'led-light-simfiles',
     description:
-      'DDR custom simfiles from the creator of StepMania Song Manager! Contains a wide variety of songs including BEMANI crossovers, K-Pop, video game OSTs, and much more!',
+      'DDR-style custom simfiles from the creator of StepMania Song Manager! Contains a wide variety of songs including BEMANI crossovers, K-Pop, video game OSTs, and much more! All charts are rated on the X-scale and synced to null offset.',
   },
   {
     name: "led_light's Tech Experiments",
     bucketName: 'led-light-simfiles-itg',
     description:
-      'ITG tech-style custom charts from the creator of StepMania Song Manager.',
+      'A selection of ITG tech-style custom charts from the creator of StepMania Song Manager! Charts are rated on the ITG scale and synced to +9ms offset.',
   },
 ]
 
