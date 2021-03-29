f1803c42db55831cae97ae436b2edf1825218bf2
Edward Ly <edward.ly@pm.me>
2021-03-29 21:03:20 +0900

    Added song list page

---
+++ src/pages/SongList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="fullscreen bg-grey-1 q-pa-lg">
+    <!-- TODO: build tables -->
+    {{ songList }}
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup () {
+    const songList = ref([])
+
+    window.electron.getSongListData((data) => songList.value = data)
+
+    return {
+      songList,
+    }
+  },
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -9,6 +9,10 @@ const routes = [
       { path: 'about', component: () => import('pages/About.vue') },
     ],
   },
+  {
+    path: '/songs',
+    component: () => import('pages/SongList.vue'),
+  },
 
   // Always leave this as last one,
   // but you can also remove it
