c35cb5885d63fedd4c973eccf15cd96d4c479ac6
Edward Ly <edward.ly@pm.me>
2021-04-02 19:47:09 +0900

    Set i18n initial locale from local storage or system

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,9 +1,20 @@
+import { Quasar, LocalStorage } from 'quasar'
 import { boot } from 'quasar/wrappers'
 import { createI18n } from 'vue-i18n'
 import messages from 'src/i18n'
+import _ from 'lodash'
+
+if (!LocalStorage.has('Locale')) {
+  const userLocale = Quasar.lang.getLocale()
+  if (_.has(messages, userLocale)) {
+    LocalStorage.set('Locale', userLocale)
+  } else {
+    LocalStorage.set('Locale', 'en-US')
+  }
+}
 
 const i18n = createI18n({
-  locale: 'en-US',
+  locale: LocalStorage.getItem('Locale'),
   messages
 })
 
