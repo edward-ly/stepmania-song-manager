1ba8b940b9d50e3c4309950b5cf6c7d838b3610d
Edward Ly <edward.ly@pm.me>
2021-04-06 12:16:08 +0900

    Temporarily disable ja locale

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,9 +1,9 @@
 import enUS from './en-US'
 import enGB from './en-GB'
-import ja from './ja'
+// import ja from './ja'
 
 export default {
   'en-US': enUS,
   'en-GB': enGB,
-  ja: ja,
+  // ja: ja,
 }
