31b4b4c6a8147cd3b4628917cd185ea0f9b2e612
Edward Ly <edward.ly@pm.me>
2021-04-08 16:21:04 +0900

    Added endpoints to default buckets

--- src/components/defaultRepos.js
+++ src/components/defaultRepos.js
@@ -2,24 +2,28 @@ const defaultRepos = [
   {
     name: 'DanceDanceRevolution (Arcade)',
     bucketName: 'ddr-arcade-simfiles',
+    endpoint: 'https://s3.us-west-000.backblazeb2.com',
     description:
       'The latest official arcade release of DDR, with all songs grouped by mix in chronological order. Will include new songs, rating adjustments, songs removals, etc. as the arcade game is updated.',
   },
   // {
   //   name: 'DanceDanceRevolution XX',
   //   bucketName: 'ddr-xx-simfiles',
+  //   endpoint: 'https://s3.us-west-000.backblazeb2.com',
   //   description:
   //     'A 20th anniversary celebration of DDR. By Zenius, for Zenius.',
   // },
   {
     name: 'The Complete led_light Collection',
     bucketName: 'led-light-simfiles',
+    endpoint: 'https://s3.us-west-000.backblazeb2.com',
     description:
       'DDR custom simfiles from the creator of StepMania Song Manager.',
   },
   {
     name: "led_light's Tech Experiments",
     bucketName: 'led-light-simfiles-itg',
+    endpoint: 'https://s3.us-west-000.backblazeb2.com',
     description:
       'ITG tech-style charts from the creator of StepMania Song Manager.',
   },
