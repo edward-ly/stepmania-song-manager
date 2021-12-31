1e9399fe194c8925e9288a8f43ff4b8bf6eb5ac4
Edward Ly <edward.ly@pm.me>
2022-01-01 01:20:26 +0900

    Added region property to repos

--- src/components/defaultRepos.js
+++ src/components/defaultRepos.js
@@ -3,6 +3,7 @@ const defaultRepos = [
     name: 'DanceDanceRevolution (Arcade)',
     bucketName: 'ddr-arcade-simfiles',
     endpoint: 'https://s3.us-west-000.backblazeb2.com',
+    region: 'us-west-000',
     description:
       'The latest official arcade release of DDR, with all songs grouped by mix in chronological order. Will include new songs, rating adjustments, songs removals, etc. as the arcade game is updated. Source files: https://zenius-i-vanisher.com/v5.2/simfiles.php?category=simfiles',
   },
@@ -10,6 +11,7 @@ const defaultRepos = [
   //   name: 'DanceDanceRevolution XX',
   //   bucketName: 'ddr-xx-simfiles',
   //   endpoint: 'https://s3.us-west-000.backblazeb2.com',
+  //   region: 'us-west-000',
   //   description:
   //     'A 20th anniversary celebration of DDR. By Zenius, for Zenius.',
   // },
@@ -17,6 +19,7 @@ const defaultRepos = [
     name: 'The Complete led_light Collection',
     bucketName: 'led-light-simfiles',
     endpoint: 'https://s3.us-west-000.backblazeb2.com',
+    region: 'us-west-000',
     description:
       'DDR custom simfiles from the creator of StepMania Song Manager. More info: https://zenius-i-vanisher.com/v5.2/viewsimfilecategory.php?categoryid=821',
   },
@@ -24,6 +27,7 @@ const defaultRepos = [
     name: "led_light's Tech Experiments",
     bucketName: 'led-light-simfiles-itg',
     endpoint: 'https://s3.us-west-000.backblazeb2.com',
+    region: 'us-west-000',
     description:
       'ITG tech-style charts from the creator of StepMania Song Manager. More info: https://zenius-i-vanisher.com/v5.2/viewsimfilecategory.php?categoryid=821',
   },
